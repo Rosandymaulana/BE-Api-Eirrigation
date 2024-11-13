90bfc5eb4628aa92337946a824fc8829bf764163
Zaed Abdullah <z4ed.thalib123@gmail.com>
2024-11-13 15:33:03 +0700

    Make phone is not required in register feature.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
             'email' => 'required',
             'password' => 'required',
             'fullname' => 'required',
-            'phone' => 'required',
+            // 'phone' => 'required',
         ]);
 
         if ($validator->fails()) {
